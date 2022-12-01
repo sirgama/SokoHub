1b5beeaeb7e7b9c1fe08d658d0db83eb1f36de9a
sirgama <sirengogamaliel@gmail.com>
2022-12-02 01:22:25 +0300

    add hero section with styling

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import HeroLanding from './components/HeroLanding'
 import NavbarLanding from './components/NavbarLanding'
 
 export default function Landing() {
   return (
     <div>
       <NavbarLanding />
+      <HeroLanding />
     </div>
   )
 }
---
+++ src/pages/Landing/components/HeroLanding.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroLanding.css'
+
+export default function HeroLanding() {
+  return (
+    <div className='hero flex flex-col justify-center align-middle items-center'>
+        <div id="overlay"></div>
+      <h3 className='text-green-50 font-extrabold text-7xl text-center p-6'>Bringing Technology to <br /> Sustainable Food Production</h3>
+      <div className="flex">
+           <div className="bg-green-900 text-white font-semibold cursor-pointer px-10 py-3 rounded-2xl mx-2 ">Learn more</div> 
+           <div className="bg-green-50 text-gray-700 font-semibold cursor-pointer px-10 py-3 rounded-2xl mx-4">Get started</div> 
+      </div>
+    </div>
+  )
+}
