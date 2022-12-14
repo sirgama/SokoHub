efa75b7960beb392ea1b07938b62825a9168c300
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-12-14 14:34:18 +0300

    add images for login and signup

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -48,35 +48,35 @@ let navigate = useNavigate()
         </div>
 
         <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
-            <p class="text-center text-3xl">Join Us.</p>
+            <p class="text-center text-3xl font-bold">Join Us.</p>
             <form class="flex flex-col pt-3 md:pt-8" onSubmit={handleSubmit}>
                 <div class="flex flex-col pt-4">
-                    <label for="name1" class="text-sm">First Name</label>
-                    <input type="text" id="name1" placeholder="John" onChange={e => setFirstname(e.target.value)} class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required/>
+                    <label for="name1" class="text-sm font-bold">First Name</label>
+                    <input type="text" id="name1" placeholder="John" onChange={e => setFirstname(e.target.value)} class="  border-t-0 border-l-0 border-r-0 border-b-2 w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required/>
                 </div>
                 <div class="flex flex-col pt-4">
-                    <label for="name2" class="text-sm">Last Name</label>
-                    <input type="text" id="name2" placeholder=" Doe" onChange={e => setLastname(e.target.value)} class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required/>
-                </div>
-
-                <div class="flex flex-col pt-4">
-                    <label for="email" class="text-sm">Email</label>
-                    <input type="email" id="email" placeholder="your@email.com" onChange={e => setEmail(e.target.value)} class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required/>
+                    <label for="name2" class="text-sm font-bold">Last Name</label>
+                    <input type="text" id="name2" placeholder=" Doe" onChange={e => setLastname(e.target.value)} class="border-t-0 border-l-0 border-r-0 border-b-2 w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required/>
                 </div>
 
                 <div class="flex flex-col pt-4">
-                    <label for="password" class="text-sm">Password</label>
-                    <input type="password" id="password" placeholder="Password" onChange={e => setPassword1(e.target.value)} class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required />
+                    <label for="email" class="text-sm font-bold">Email</label>
+                    <input type="email" id="email" placeholder="your@email.com" onChange={e => setEmail(e.target.value)} class="border-t-0 border-l-0 border-r-0 border-b-2 w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required/>
                 </div>
 
                 <div class="flex flex-col pt-4">
-                    <label for="confirm-password" class="text-sm">Confirm Password</label>
-                    <input type="password" id="confirm-password" placeholder="Confirm Password" onChange={e => setPassword2(e.target.value)} class="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required />
+                    <label for="password" class="text-sm font-bold">Password</label>
+                    <input type="password" id="password" placeholder="Password" onChange={e => setPassword1(e.target.value)} class="border-t-0 border-l-0 border-r-0 border-b-2 w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required />
+                </div>
+
+                <div class="flex flex-col pt-4">
+                    <label for="confirm-password" class="text-sm font-bold">Confirm Password</label>
+                    <input type="password" id="confirm-password" placeholder="Confirm Password" onChange={e => setPassword2(e.target.value)} class="border-t-0 border-l-0 border-r-0 border-b-2 w-full py-1 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" required />
                 </div>
 
                 <div className="flex flex-col pt-4">
-                <label for="role" class="text-sm">Register as:</label>
-                <select defaultValue={role} onChange={e => setRole(e.target.value)} class="border rounded w-full py-1 px-3 text-gray-700 mt-1 leading-tight " name="role" id="role">
+                <label for="role" class="text-sm font-bold">Register as:</label>
+                <select defaultValue={role} onChange={e => setRole(e.target.value)} class="border-t-0 border-l-0 border-r-0 border-b-2 w-full py-1 px-3 text-gray-700 mt-1 leading-tight " name="role" id="role">
                     <option value="FARMER">Farmer</option>
                     <option value="INVESTOR">Investor</option>
                     <option value="LEASER">Leaser</option>
@@ -84,7 +84,7 @@ let navigate = useNavigate()
                 </select>
                 </div>
 
-                <input type="submit" value="Register" class="bg-green-800 text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" />
+                <input type="submit" value="Register" class="bg-sky-800 text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" />
             </form>
             <div class="text-center pt-12 pb-12">
                 <p>Already have an account? <Link to="/auth/login" class="underline font-semibold">Login here.</Link></p>
